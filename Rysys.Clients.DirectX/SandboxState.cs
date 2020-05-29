24ef62173cf9e2a42742517a4f30e7b9c1ddee94
Ryan Fernung <fernung@outlook.com>
2020-05-29 12:33:45 -0400

    Added base player class to inherit

--- Rysys.Clients.DirectX/SandboxState.cs
+++ Rysys.Clients.DirectX/SandboxState.cs
@@ -13,14 +13,14 @@ namespace Rysys.Clients.DirectX
 {
     public class SandboxState : GameState
     {
-        public IActor player1;
-        public IActor player2;
+        public IPlayer player1;
+        public IPlayer player2;
 
 
         public SandboxState() : base() 
         {
-            player1 = new Actor(ActorType.Player);
-            player2 = new Actor(ActorType.Player);
+            player1 = new Player(ActorType.Player, PlayerIndex.One);
+            player2 = new Player(ActorType.Player, PlayerIndex.Two);
 
             Components.Add(player1);
             Components.Add(player2);
@@ -29,18 +29,11 @@ namespace Rysys.Clients.DirectX
         public override void Initialize()
         {
             player1.Initialize();
-            player1.AddComponent(new MouseInput());
-            player1.AddComponent(new KeyboardInput());
-            player1.AddComponent(new GamePadInput(PlayerIndex.One));
             player1.GetComponent<Kinematics>().Position = new Vector2(Settings.Width / 2, Settings.Height / 2) - player1.GetComponent<Sprite>().Size;
             player1.GetComponent<Sprite>().Color = Color.Red;
 
             player2.Initialize();
-            player2.AddComponent(new MouseInput());
-            player2.AddComponent(new KeyboardInput());
-            player2.AddComponent(new GamePadInput(PlayerIndex.Two));
             player2.GetComponent<Kinematics>().Position = new Vector2(Settings.Width / 2, Settings.Height / 2) + player2.GetComponent<Sprite>().Size;
-            player2.GetComponent<Kinematics>().Speed = 4.0f;
             player2.GetComponent<Sprite>().Color = Color.Blue;
 
             base.Initialize();
@@ -53,22 +46,11 @@ namespace Rysys.Clients.DirectX
             if (player1.GetComponent<GamePadInput>().Pressed(Buttons.Back) ||
                 player1.GetComponent<KeyboardInput>().Pressed(Keys.Escape))
                 GameStateManager.RequestExit = true;
-
-            Vector2 input = player1.GetComponent<GamePadInput>().LeftStickDirection() * player1.GetComponent<Kinematics>().Speed;
-            player1.GetComponent<Kinematics>().Accelerate(input.X, input.Y);
-
-            input = player2.GetComponent<GamePadInput>().LeftStickDirection() * player2.GetComponent<Kinematics>().Speed;
-            player2.GetComponent<Kinematics>().Accelerate(input.X, input.Y);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             base.Draw(spriteBatch);
-
-            spriteBatch.Begin();
-            player1.Draw(spriteBatch);
-            player2.Draw(spriteBatch);
-            spriteBatch.End();
         }
     }
 }
---
+++ Rysys/Actors/IPlayer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Rysys.Input;
+using Rysys.Physics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rysys.Actors
+{
+    public interface IPlayer : IActor
+    {
+
+    }
+
+    public class Player : Actor, IPlayer
+    {
+        public PlayerIndex Index { get; protected set; }
+
+        public Player(ActorType type, PlayerIndex index) : base(type)
+        {
+            Index = index;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            AddComponent(new MouseInput());
+            AddComponent(new KeyboardInput());
+            AddComponent(new GamePadInput(Index));
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            Vector2 input = GetComponent<GamePadInput>().LeftStickDirection() * GetComponent<Kinematics>().Speed;
+            GetComponent<Kinematics>().Accelerate(input.X, input.Y);
+        }
+    }
+}
